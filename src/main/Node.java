d3aba590a7cf352b8fe645d037ff1b1c3fc7d5be
sahildanger <sahil.chauhan_ccv17@gmail.com>
2019-02-20 11:17:40 +0530

    add getter setter for node

--- src/main/Node.java
+++ src/main/Node.java
@@ -2,12 +2,21 @@ package main;
 
 public class Node {
 private int data;
-
+private Node next;
     public int getData() {
         return data;
     }
 
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
     public void setData(int data) {
         this.data = data;
+
     }
 }
